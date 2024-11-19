80e7c4f5a8b862ea172f9d97cf0b49b9c2c5bbe8
Aswin Anand <aswin.anand@synaptics.com>
2024-11-19 01:30:59 -0800

    recipes-devtools: isp_media_server: Change default sensor to OV5647
    Change-Id: I5b139b5d3afee70cd7075c75f8ba804e8a971900
    Reviewed-on: https://gerrit-sha.synaptics.com/gerrit/246501
    Reviewed-by: Rajdeep Shyam <Rajdeep.Shyam@synaptics.com>
    Reviewed-by: Sujatha Rajaraman <Sujatha.Rajaraman@synaptics.com>

--- recipes-devtools/synasdk/files/isp_media_server.sh
+++ recipes-devtools/synasdk/files/isp_media_server.sh
@@ -26,7 +26,7 @@ set -e
 
 case $1 in
     start)
-        echo "sensor=imx258 xml=/usr/share/IMX258.xml manu_json=/usr/share/ISP_Manual_IMX258.json \
+        echo "sensor=ov5647 xml=/usr/share/OV5647_480p.xml manu_json=/usr/share/ISP_Manual_IMX258.json \
         auto_json=/usr/share/ISP_Auto.json i2c_bus_id=3 mipi_id=0 mode=0" > /proc/vsi/isp_subdev0
         echo -n "Starting $DESC: "
         start-stop-daemon --start $SSD_OPTIONS  > $LOGFILE &
