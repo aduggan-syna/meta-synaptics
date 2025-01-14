aduggan-syna/meta-synaptics

commit 74f2041fe78719d496be0bd740c8717ce36625c6

synasdk: add vpu TA and plugin bb file
JIRA: https://synaptics.atlassian.net/browse/VSSDK-32370

Change-Id: I6494df55fdfebbb4b5b85eadb844b3e3f365faaf
Reviewed-on: https://gerrit-sha.synaptics.com/gerrit/250926
Reviewed-by: Zhe Tong <Zhe.Tong@synaptics.com>
Reviewed-by: Yanyan Peng <Yanyan.Peng@synaptics.com>
Reviewed-by: Shanzhi Wang <Shanzhi.Wang@synaptics.com>

recipes-security/optee/optee-os_4.0.0.bbappend
@@ -10,6 +10,7 @@ SRCREV_FORMAT = "taenc"
 DEPENDS:append = " \
     synasdk-tools-native \
     synasdk-security-native \
+    synasdk-vpu-ta \
 "
 
 SYNA_TA_PATH = "${WORKDIR}/${SYNA_SOURCE_PREFIX}/ta_enc"
@@ -21,13 +22,13 @@ EARLY_SYNA_TA:platypus=" ${SYNA_TA_PATH}/libfastlogo.ta/platypus/A0/1316a183-894
                          ${SYNA_TA_PATH}/libgfx_img_linux.ta/platypus/A0/1316a183-894d-43fe-9893-bb946ae10436.stripped.elf \
                          ${SYNA_TA_PATH}/libptm.ta/platypus/A0/1316a183-894d-43fe-9893-bb946ae1042d.stripped.elf \
                          ${SYNA_TA_PATH}/libsynap.ta/platypus/A0/1316a183-894d-43fe-9893bb946ae1042f.stripped.elf \
-                         ${SYNA_TA_PATH}/libvmeta.ta/platypus/A0/1316a183-894d-43fe-9893-bb946ae103f0.stripped.elf \
+                         ${STAGING_BASELIBDIR}/optee_armtz/1316a183-894d-43fe-9893-bb946ae103f0.stripped.elf \
 "
 EARLY_SYNA_TA:dolphin=" ${SYNA_TA_PATH}/libfastlogo.ta/dolphin/A0/genx/1316a183-894d-43fe-9893-bb946ae103f5.stripped.elf \
                         ${SYNA_TA_PATH}/libgfx_img_linux.ta/dolphin/A0/genx/1316a183-894d-43fe-9893-bb946ae10436.stripped.elf \
                         ${SYNA_TA_PATH}/libptm.ta/dolphin/A0/genx/1316a183-894d-43fe-9893-bb946ae1042d.stripped.elf \
                         ${SYNA_TA_PATH}/libsynap.ta/dolphin/A0/genx/1316a183-894d-43fe-9893bb946ae1042f.stripped.elf \
-                        ${SYNA_TA_PATH}/libvmeta.ta/dolphin/A0/genx/1316a183-894d-43fe-9893-bb946ae103f0.stripped.elf \
+                        ${STAGING_BASELIBDIR}/optee_armtz/1316a183-894d-43fe-9893-bb946ae103f0.stripped.elf \
                         ${SYNA_TA_PATH}/libdhub.ta/dolphin/A0/genx/1316a183-894d-43fe-9893-bb946ae103f3.stripped.elf \
 "
 EARLY_TA_CFG = " CFG_EARLY_TA=y EARLY_TA_PATHS="${EARLY_SYNA_TA}""

recipes-bsp/images/astra-media-common.inc
@@ -176,12 +176,14 @@ IMAGE_INSTALL:append:dolphin = " \
     ${PLATYPUS_DOLPHIN_INSTALL} \
     synasdk-v4l2isp-daemon \
     synasdk-drivers-hrx-v4l2 \
+    synasdk-vpu-plugin \
 "
 
 IMAGE_INSTALL:append:platypus = " \
     ${PLATYPUS_DOLPHIN_INSTALL} \
     synasdk-drivers-sunplus \
     synasdk-drivers-syna-dvfs \
+    synasdk-vpu-plugin \
 "
 
 TOOLCHAIN_TARGET_TASK:append = " synasdk-synap-framework-staticdev kernel-devsrc"

recipes-devtools/synasdk/synasdk-vpu-plugin_git.bb
@@ -1,0 +1,25 @@
+SUMMARY = "Synaptics VPU Plugin"
+SECTION = "devtools"
+LICENSE = "CLOSED"
+LICENSE_FLAGS = "Synaptics-EULA"
+
+require recipes-security/optee/optee.inc
+
+PLUGIN_UUID = "1316a183-894d-43fe-9893-bb946ae103f0"
+
+DEPENDS = "optee-os-tadevkit optee-client"
+
+SRC_URI = "${SYNA_SRC_OPTEE_DEV}"
+
+S = "${WORKDIR}/${SYNA_SOURCE_PREFIX}/tee/optee_dev/ta/vpu/plugin"
+
+do_compile() {
+    oe_runmake -C ${S}
+}
+
+do_install() {
+    install -d ${D}${libdir}/tee-supplicant/plugins
+    install -m 0755 ${S}/${PLUGIN_UUID}.plugin ${D}${libdir}/tee-supplicant/plugins
+}
+
+FILES:${PN} = "${libdir}/tee-supplicant/plugins/"

recipes-devtools/synasdk/synasdk-vpu-ta_git.bb
@@ -1,0 +1,34 @@
+SUMMARY = "Synaptics VPU TA"
+SECTION = "devtools"
+LICENSE = "CLOSED"
+LICENSE_FLAGS = "Synaptics-EULA"
+
+require recipes-security/optee/optee.inc
+
+inherit python3native
+
+DEPENDS = "python3-cryptography-native optee-os-tadevkit"
+
+SRC_URI = "${SYNA_SRC_OPTEE_DEV}"
+
+S = "${WORKDIR}/${SYNA_SOURCE_PREFIX}/tee/optee_dev/ta/vpu"
+B = "${S}/ta"
+OUT = "${WORKDIR}/OUT"
+
+CHIP_FULL_NAME:platypus = "platypus"
+CHIP_FULL_NAME:dolphin = "dolphin_a0"
+
+EXTRA_OEMAKE += "TA_DEV_KIT_DIR=${TA_DEV_KIT_DIR} \
+                 CHIP_FULL_NAME=${CHIP_FULL_NAME} \
+                 CROSS_COMPILE=${HOST_PREFIX} \
+                 TEE_CATEGORY=OPTEE \
+                 O=${OUT}"
+
+do_install() {
+    install -d ${D}${base_libdir}/optee_armtz/
+    install -m 0755 ${OUT}/*.ta ${D}${base_libdir}/optee_armtz/
+    install -m 0755 ${OUT}/*.elf ${D}${base_libdir}/optee_armtz/
+}
+
+FILES:${PN} = "${base_libdir}/optee_armtz/"
+INSANE_SKIP:${PN} += " already-stripped"

commit b2bc1a29a85735c4348b637f790d2768bff10601

synasdk: Fix build error of sl1620 profile
- It's caused by CL 250926.

Change-Id: I396cf91fe2974fc344feb75df50399034f71764f
Reviewed-on: https://gerrit-sha.synaptics.com/gerrit/252432
Reviewed-by: Zhe Tong <Zhe.Tong@synaptics.com>
Reviewed-by: Yanyan Peng <Yanyan.Peng@synaptics.com>
Reviewed-by: Shanzhi Wang <Shanzhi.Wang@synaptics.com>

recipes-security/optee/optee-os_4.0.0.bbappend
@@ -10,8 +10,10 @@ SRCREV_FORMAT = "taenc"
 DEPENDS:append = " \
     synasdk-tools-native \
     synasdk-security-native \
-    synasdk-vpu-ta \
 "
+
+DEPENDS:append:dolphin = "synasdk-vpu-ta"
+DEPENDS:append:platypus = "synasdk-vpu-ta"
 
 SYNA_TA_PATH = "${WORKDIR}/${SYNA_SOURCE_PREFIX}/ta_enc"
 